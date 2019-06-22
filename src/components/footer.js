eed51fdfed50e644cbe96154edc458430fead29e
luozhe73 <1043269439@qq.com>
2019-06-22 11:15:12 +0800

    react init

---
+++ src/components/footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class ComponentFooter extends React.Component {
+
+    render() {
+        return (
+            <header>
+                <h1>这里是尾部</h1>
+            </header>
+        )
+    }
+}
+
+export default ComponentFooter;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ComponentHeader from './components/header';
+import ComponentFooter from './components/footer';
+import BodyIndex from './components/BodyIndex';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Index extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <ComponentHeader/>
+                <BodyIndex/>
+                <ComponentFooter/>
+            </div>
+
+        )
+    }
+}
+
+ReactDOM.render(<Index/>, document.getElementById('root'));
